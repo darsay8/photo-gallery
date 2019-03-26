6e5704432074d3ed51d9f760ed84cec579b44060
darsay8 <darsay.one@gmail.com>
2019-03-26 11:39:28 -0300

    infite scroll added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-import React, {Component} from 'react';
-import './App.css';
-import Images from './components/Images'
+import React, { Component } from "react";
+import "./App.css";
+import Images from "./components/Images";
 
 class App extends Component {
-
-    render() {
-        return (
-            <div className="App">
-
-                <div className="hero-body">
-                    <div className="container">
-                        <div className="header content">
-                            <h2 className="subtitle is-4">The most awesome photos around the world</h2>
-                            <h1 className="title is-1">
-                                Beautiful Images <span role="img" aria-label="camera">📸</span>
-                            </h1>
-                        </div>
-                        <Images/>
-                    </div>
-                </div>
+  render() {
+    return (
+      <div className="App">
+        <div className="hero-body">
+          <div className="container">
+            <div className="header content">
+              <h2 className="subtitle is-4">
+                The most awesome photos around the world
+              </h2>
+              <h1 className="title is-1">
+                Beautiful Images{" "}
+                <span role="img" aria-label="camera">
+                  📸
+                </span>
+              </h1>
             </div>
-        );
-    }
+            <Images />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Images.js
+++ src/components/Images.js
@@ -1,41 +1,54 @@
-import React, {Component} from 'react';
-import axios from 'axios';
-import Image from './Image';
+import React, { Component } from "react";
+import axios from "axios";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Image from "./Image";
 
 class Images extends Component {
+  state = {
+    images: [],
+    categories: [],
+    start: 1,
+    count: 30
+  };
 
-    state = {
-        images: [],
-        categories: [],
-        start: 1,
-        count: 30,
-    };
+  componentDidMount() {
+    const { count, start } = this.state;
+    this.setState({ start: this.state.start + count });
+    console.log("componentDidMount:" + count, start);
+    axios
+      .get(`/api/photos?count=${count}&start=${start}`)
+      .then(res => this.setState({ images: res.data }));
+  }
 
-    componentDidMount() {
-        const {count, start} = this.state;
-        axios
-            .get(`/api/photos?count=${count}&start=${start}`)
-            .then(res => this.setState({images: res.data}));
-    }
+  loadMore = () => {
+    const { count, start } = this.state;
+    this.setState({ start: this.state.start + count });
+    console.log("loadMore:" + count, start);
+    axios
+      .get(`/api/photos?count=${count}&start=${start}`)
+      .then(res =>
+        this.setState({ images: this.state.images.concat(res.data) })
+      );
+  };
 
-    render() {
-
-        return (
-            <div>
-                <div className="image-grid">
-                    {
-                        this.state.images.map((image) => (
-                            <Image
-                                image={image}
-                                key={image.id}
-                            />
-
-                        ))
-                    }
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <InfiniteScroll
+          dataLength={this.state.images.length}
+          next={this.loadMore}
+          hasMore={true}
+          loader={<h4>Loading...</h4>}
+        >
+          <div className="image-grid">
+            {this.state.images.map(image => (
+              <Image image={image} key={image.id} />
+            ))}
+          </div>
+        </InfiniteScroll>
+      </div>
+    );
+  }
 }
 
 export default Images;
